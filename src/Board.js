51f2af5565fff9140d4d514366462ea80450e56e
mfadel85 <faddel.homsi@gmail.com>
2020-12-28 11:30:42 +0300

    showing prdocuts in the board

--- src/Board.js
+++ src/Board.js
@@ -20,6 +20,140 @@ class Board extends React.Component {
         this.state = {
             cells: Array(110).fill(null),
             xIsNext: true,
+            products: [
+                {
+                    id: 1,
+                    name: 'Pepsi',
+                    symbol: 'P',
+                    dir: 'left',
+                    width: 10,
+                    depth: 10,
+                    beltCount: 2,
+                    cellsDepth: 4,
+                    unitNo: 1
+                },
+                {
+                    id: 2,
+                    name: 'Browni',
+                    symbol: 'B',
+                    dir: 'right',
+                    width: 12,
+                    depth: 6,
+                    beltCount: 2,
+                    cellsDepth: 3,
+                    unitNo: 2
+                },
+                {
+                    id: 3,
+                    name: 'Jucy',
+                    symbol: 'J',
+                    dir: 'right',
+                    width: 6.2,
+                    depth: 9.3,
+                    beltCount: 1,
+                    cellsDepth: 4,
+                    unitNo: 1
+                },
+                {
+                    id: 4,
+                    name: 'Milk',
+                    symbol: 'M',
+                    dir: 'right',
+                    width: 6.6,
+                    depth: 5,
+                    beltCount: 1,
+                    cellsDepth: 2,
+                    unitNo: 2
+                },
+                {
+                    id: 5,
+                    name: 'Islak Mendil',
+                    symbol: 'I',
+                    dir: 'left',
+                    width: 9.5,
+                    depth: 25,
+                    beltCount: 2,
+                    cellsDepth: 10,
+                    unitNo: 3
+                },
+                {
+                    id: 6,
+                    name: 'Tooth Paste',
+                    symbol: 'T',
+                    dir: 'left',
+                    width: 20,
+                    depth: 3.6,
+                    beltCount: 3,
+                    cellsDepth: 2,
+                    unitNo: 3
+                },
+                {
+                    id: 7,
+                    name: 'Cafe Crown',
+                    symbol: 'C',
+                    dir: 'right',
+                    width: 7,
+                    depth: 10.5,
+                    beltCount: 1,
+                    cellsDepth: 5,
+                    unitNo: 4
+                },
+                {
+                    id: 8,
+                    name: 'Gofret',
+                    symbol: 'G',
+                    dir: 'left',
+                    width: 18,
+                    depth: 3.5,
+                    beltCount: 3,
+                    cellsDepth: 2,
+                    unitNo: 1
+                },
+                {
+                    id: 9,
+                    name: 'Çıkolata',
+                    symbol: 'Ç',
+                    dir: 'left',
+                    width: 13,
+                    depth: 9.2,
+                    beltCount: 2,
+                    cellsDepth: 4,
+                    unitNo: 3
+                },
+                {
+                    id: 10,
+                    name: 'Domates Salçası',
+                    symbol: 'D',
+                    dir: 'right',
+                    width: 10,
+                    depth: 10,
+                    beltCount: 2,
+                    cellsDepth: 4,
+                    unitNo: 4
+                },
+                {
+                    id: 11,
+                    name: 'Ülker Laviva',
+                    symbol: 'Ü',
+                    dir: 'left',
+                    width: 15,
+                    depth: 2.6,
+                    beltCount: 3,
+                    cellsDepth: 2,
+                    unitNo: 1
+                },
+                {
+                    id: 12,
+                    name: 'Coka Cola',
+                    symbol: 'K',
+                    dir: 'left',
+                    width: 5.8,
+                    depth: 5.8,
+                    beltCount: 1,
+                    cellsDepth: 3,
+                    unitNo: 1
+                },
+            ]
         };
     }
 
@@ -46,7 +180,10 @@ class Board extends React.Component {
         });
         return (
             <Row>
-                <Col><Products /></Col>
+                <Col> 
+                    <h1>Products </h1>
+                    <Products products={ this.state.products } />
+                </Col>
                 <Col>1 of 3</Col>
                 <Col>
 
