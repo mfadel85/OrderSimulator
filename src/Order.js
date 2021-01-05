148b65d2c2f4b91e85b8e030cd973d88cc351455
mfadel85 <faddel.homsi@gmail.com>
2021-01-05 10:33:26 +0300

    delete not used codes

--- src/Order.js
+++ src/Order.js
@@ -1,18 +1,6 @@
 import React from 'react';
 
 class Order extends React.Component {
-    /*constructor(props) {
-        super(props);
-        //console.log('before everything', this.props.order);
-        this.state = {   
-            order : this.props.order,
-            products: this.props.products
-        }
-    }*/
-/*
-             <h6 key={product.name.id} >{index+1}. { product.name.name}  - { product.quantity} pcs - { product.name.dir}  - { product.name.beltCount}  - { product.name.cellsDepth} Depth </h6>
-
-*/
     render() {
         return this.props.order.map((product,index) => (
             <tr className='orderItem' key={product.name.id} >
@@ -23,10 +11,8 @@ class Order extends React.Component {
                 <td>{product.name.beltCount}</td>
                 <td>{product.name.cellsDepth}</td>
                 <td>{product.name.unitNo}</td>
-
             </tr>
         ))
     };
 }
-
 export default Order;
